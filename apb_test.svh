2ad63eccaa21dfbedabda601cd2a08fbe3871e4d
robotmir <robotmir@gmail.com>
2023-04-13 17:45:37 -0400

    commented to try to test for compile

--- apb_test.svh
+++ apb_test.svh
@@ -6,34 +6,35 @@ class test extends uvm_test;
   `uvm_component_utils(test)
 
   environment env; // top level environment
-  virtual counter_if vif; // virtual interface
+  virtual apb_if apb_vif; // virtual interface
+  virtual bus_protocol_if bus_vif; // virtual interface
   counter_sequence seq; // top level sequence
 
-  function new(string name = "test", uvm_component parent);
-		super.new(name, parent);
-	endfunction: new
+  // function new(string name = "test", uvm_component parent);
+	// 	super.new(name, parent);
+	// endfunction: new
 
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-		env = environment::type_id::create("env",this);
-    seq = counter_sequence::type_id::create("seq");
+  // function void build_phase(uvm_phase phase);
+  //   super.build_phase(phase);
+	// 	env = environment::type_id::create("env",this);
+  //   seq = counter_sequence::type_id::create("seq");
 
-    // send the interface down
-    if (!uvm_config_db#(virtual counter_if)::get(this, "", "counter_vif", vif)) begin 
-      // check if interface is correctly set in testbench top level
-		   `uvm_fatal("TEST", "No virtual interface specified for this test instance")
-		end 
+  //   // send the interface down
+  //   if (!uvm_config_db#(virtual counter_if)::get(this, "", "counter_vif", vif)) begin 
+  //     // check if interface is correctly set in testbench top level
+	// 	   `uvm_fatal("TEST", "No virtual interface specified for this test instance")
+	// 	end 
 
-		uvm_config_db#(virtual counter_if)::set(this, "env.agt*", "counter_vif", vif);
+	// 	uvm_config_db#(virtual counter_if)::set(this, "env.agt*", "counter_vif", vif);
 
-  endfunction: build_phase
+  // endfunction: build_phase
 
-  task run_phase(uvm_phase phase);
-    phase.raise_objection( this, "Starting sequence in main phase" );
-		$display("%t Starting sequence run_phase",$time);
- 		seq.start(env.agt.sqr);
-		#100ns;
-		phase.drop_objection( this , "Finished in main phase" );
-  endtask
+  // task run_phase(uvm_phase phase);
+  //   phase.raise_objection( this, "Starting sequence in main phase" );
+	// 	$display("%t Starting sequence run_phase",$time);
+ 	// 	seq.start(env.agt.sqr);
+	// 	#100ns;
+	// 	phase.drop_objection( this , "Finished in main phase" );
+  // endtask
 
 endclass: test
--- tb_completer.sv
+++ tb_completer.sv
@@ -20,23 +20,24 @@ module tb_completer ();
   // generate clock
   initial begin
     PCLK = 0;
-    forever #10 clk = !clk;
+    forever #10 PCLK = !PCLK;
   end
 
-
+  PRESETn = 1'b0;
 
   // instantiate the interface
-    ///////////////////////////////////////////////////// edit
-  apb_if apb_dut_if(PCLK, PRESETn);
-  bus_protocol_if bus_dut_if();
+  apb_if apb_slave_if(PCLK, PRESETn);
+  bus_protocol_if bus_slave_if();
   
+    ///////////////////////////////////////////////////// edit
   // instantiate the DUT
-  apb_completer apb(apb_dut_if.apb);
+  apb_completer apb(apb_slave_if.apb);
+  apb_completer apb(bus_slave_if.apb);
 
   // start the test
   initial begin
-    // uvm_config_db#(virtual counter_if)::set( null, "", "vif", apb_dut_if); // configure the interface into the database, so that it can be accessed throughout the hierachy
-    uvm_config_db#(virtual apb_if)::set( null, "", "vif", apb_dut_if); // configure the interface into the database, so that it can be accessed throughout the hierachy
+    // uvm_config_db#(virtual counter_if)::set( null, "", "vif", apb_slave_if); // configure the interface into the database, so that it can be accessed throughout the hierachy
+    // uvm_config_db#(virtual apb_if)::set( null, "", "vif", apb_slave_if); // configure the interface into the database, so that it can be accessed throughout the hierachy
     run_test("test"); // initiate test component
     // you can also call run_test() without argument and specify the test name by using command line option +UVM_TESTNAME="test"
   end
